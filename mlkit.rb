e72aacfbb5a914db4b49bb450098c9e512ec0e7d
Jack Nagel <jacknagel@gmail.com>
2014-12-05 01:15:50 -0500

    mlkit: modernize autotools deps

--- mlkit.rb
+++ mlkit.rb
@@ -5,7 +5,7 @@ class Mlkit < Formula
   url 'https://downloads.sourceforge.net/project/mlkit/mlkit-4.3.7/mlkit-4.3.7.tgz'
   sha1 '7c1f69f0cde271f50776d33b194699b403bab598'
 
-  depends_on :autoconf => :build
+  depends_on "autoconf" => :build
   depends_on 'mlton' => :build
   depends_on :tex
   depends_on 'gmp'
