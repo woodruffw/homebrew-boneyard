c89747ba5f077a4a13d767fae12f4eb8857cc15f
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-07 14:30:02 +0100

    woof: migrate to core
    Closes #72.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- woof.rb
+++
@@ -1,8 +0,0 @@
-require 'formula'
-
-class Woof < ScriptFileFormula
-  url "http://www.home.unix-ag.org/simon/woof"
-  version '20091227'
-  sha1 '5070c95601ceefb358af534f2d8ec86d80a28f46'
-  homepage 'http://www.home.unix-ag.org/simon/woof.html'
-end
