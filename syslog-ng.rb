7f06cec798c3c2f45d5c3f8beb738675cc529573
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-30 13:31:31 +0100

    syslog-ng: desc added

--- syslog-ng.rb
+++ syslog-ng.rb
@@ -1,17 +1,16 @@
-require 'formula'
-
 class SyslogNg < Formula
-  homepage 'http://www.balabit.com/network-security/syslog-ng/'
-  url 'http://www.balabit.com/downloads/files/syslog-ng/sources/3.2.5/source/syslog-ng_3.2.5.tar.gz'
-  sha1 '5541cd6711b7a9d983601d8047b9a27d98ecbe9b'
+  desc "Log management infrastructure"
+  homepage "http://www.balabit.com/network-security/syslog-ng/"
+  url "http://www.balabit.com/downloads/files/syslog-ng/sources/3.2.5/source/syslog-ng_3.2.5.tar.gz"
+  sha256 "ffc9f3a0ebea836c1c737b1ff49efe731d885af1d8aacf9eca79d9144eeefa89"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'pcre'
-  depends_on 'eventlog'
-  depends_on 'glib'
+  depends_on "pkg-config" => :build
+  depends_on "pcre"
+  depends_on "eventlog"
+  depends_on "glib"
 
   def install
-    ENV.append 'LDFLAGS', '-levtlog -lglib-2.0' # help the linker find symbols
+    ENV.append "LDFLAGS", "-levtlog -lglib-2.0" # help the linker find symbols
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--enable-dynamic-linking",
@@ -22,8 +21,8 @@ class SyslogNg < Formula
     # the HAVE_ENVIRON check in configure fails
     # discussion for a fix is ongoing on the Homebrew mailing list, but for
     # now this is sufficient
-    inreplace 'config.h', '#define HAVE_ENVIRON 1', ''
+    inreplace "config.h", '#define HAVE_ENVIRON 1', ""
 
-    system "make install"
+    system "make", "install"
   end
 end
