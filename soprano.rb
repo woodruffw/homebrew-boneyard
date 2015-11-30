4b60addedcfa126b52dd16c60c79d956585638ed
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-30 14:17:31 +0100

    soprano: desc added

--- soprano.rb
+++ soprano.rb
@@ -1,20 +1,19 @@
-require 'formula'
-
 class Soprano < Formula
-  homepage 'http://soprano.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/soprano/Soprano/2.6.0/soprano-2.6.0.tar.bz2'
-  sha1 '72cc2e55597adbf9d2d0b416171d854fddbde503'
+  desc "Object-oriented C++/Qt4 framework for RDF data"
+  homepage "http://soprano.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/soprano/Soprano/2.6.0/soprano-2.6.0.tar.bz2"
+  sha256 "02f754662cff357d54f4e978f648574e3c29b8c4235c3e76e5b388cd2061cd35"
 
-  depends_on 'cmake' => :build
-  depends_on 'qt'
-  depends_on 'clucene' => :optional
-  depends_on 'raptor' => :optional
-  depends_on 'redland' => :optional
+  depends_on "cmake" => :build
+  depends_on "qt"
+  depends_on "clucene" => :optional
+  depends_on "raptor" => :optional
+  depends_on "redland" => :optional
 
   def install
-    ENV['CLUCENE_HOME'] = HOMEBREW_PREFIX
+    ENV["CLUCENE_HOME"] = HOMEBREW_PREFIX
 
-    system "cmake #{std_cmake_parameters} ."
-    system "make install"
+    system "cmake", ".", *std_cmake_parameters
+    system "make", "install"
   end
 end
