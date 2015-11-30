539a37405c2bd9bdb80b2d14595683b51f8c8ba6
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-30 13:37:53 +0100

    strigi: desc added

--- strigi.rb
+++ strigi.rb
@@ -1,19 +1,20 @@
-require 'formula'
-
 class Strigi < Formula
-  homepage 'http://strigi.sourceforge.net/'
-  url 'http://www.vandenoever.info/software/strigi/strigi-0.7.2.tar.bz2'
-  sha1 'b4c1472ef068536acf9c5c4c8f033a97f9c69f9f'
+  desc "Program for fast indexing and searching personal data"
+  homepage "http://www.vandenoever.info/software/strigi/"
+  url "http://www.vandenoever.info/software/strigi/strigi-0.7.2.tar.bz2"
+  sha256 "1ca421bbe420d1839bbe6bfe7c4b52a05590a9b7a7eda77924b554c98e602ab8"
 
-  depends_on 'cmake' => :build
-  depends_on 'clucene'
-  depends_on 'exiv2' => :optional
+  depends_on "cmake" => :build
+  depends_on "clucene"
+  depends_on "exiv2" => :optional
 
   def install
-    ENV['CLUCENE_HOME'] = HOMEBREW_PREFIX
-    ENV['EXPAT_HOME'] = '/usr/'
+    ENV["CLUCENE_HOME"] = HOMEBREW_PREFIX
+    ENV["EXPAT_HOME"] = "/usr/"
 
-    system "cmake #{std_cmake_parameters} -DENABLE_EXPAT:BOOL=ON -DENABLE_DBUS:BOOL=OFF ."
-    system "make install"
+    system "cmake", ".", "-DENABLE_EXPAT:BOOL=ON",
+                         "-DENABLE_DBUS:BOOL=OFF",
+                         *std_cmake_parameters
+    system "make", "install"
   end
 end
