da671f2002a7fba7895a3d17873de761e1e1950a
Baptiste Fontaine <batifon@yahoo.fr>
2015-12-01 14:39:38 +0100

    shared-desktop-ontologies: desc added

--- shared-desktop-ontologies.rb
+++ shared-desktop-ontologies.rb
@@ -1,14 +1,13 @@
-require 'formula'
-
 class SharedDesktopOntologies < Formula
-  homepage 'http://sourceforge.net/apps/trac/oscaf/'
-  url 'https://downloads.sourceforge.net/project/oscaf/shared-desktop-ontologies/0.5/shared-desktop-ontologies-0.5.tar.bz2'
-  sha1 '672e10ba113314083b43702d9456a9c92e4f501f'
+  desc "RDF vocabularies for the Semantic Desktop"
+  homepage "http://sourceforge.net/apps/trac/oscaf/"
+  url "https://downloads.sourceforge.net/project/oscaf/shared-desktop-ontologies/0.5/shared-desktop-ontologies-0.5.tar.bz2"
+  sha256 "f904792a4a9f0b5a5a6c7e5e09f4ebc741d617b69bff7d5e1e23903096d02fa8"
 
-  depends_on 'cmake' => :build
+  depends_on "cmake" => :build
 
   def install
-    system "cmake #{std_cmake_parameters} ."
-    system "make install"
+    system "cmake", ".", *std_cmake_parameters
+    system "make", "install"
   end
 end
