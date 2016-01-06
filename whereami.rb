f4574502f4c6366e9698bfb0f91ef5acb4d74d1b
Baptiste Fontaine <batifon@yahoo.fr>
2016-01-06 18:05:35 +0100

    whereami: desc fixed

--- whereami.rb
+++ whereami.rb
@@ -1,5 +1,5 @@
 class Whereami < Formula
-  desc "Provides user's current location from the command-line"
+  desc "Provides user's current location from the CLI"
   homepage "https://victor.github.io/whereami/"
   url "https://github.com/victor/whereami/archive/v1.0.tar.gz"
   sha256 "75efa77897102e657ab6d23c97222dea4e7a2c9a1702664fd56506a7a187cf62"
