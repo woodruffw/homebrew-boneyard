0b3aad165d1ccd9f582a7f1c3d9eb14bfb8395b0
Baptiste Fontaine <batifon@yahoo.fr>
2016-01-06 18:03:11 +0100

    wps2odt: shorter desc

--- wps2odt.rb
+++ wps2odt.rb
@@ -1,5 +1,5 @@
 class Wps2odt < Formula
-  desc "Library for importing MS Works word processor file format"
+  desc "Library for importing MS Works file format"
   homepage "http://libwps.sourceforge.net"
   url "https://downloads.sourceforge.net/project/libwps/wps2odt/wps2odt-0.2.0/wps2odt-0.2.0.tar.bz2"
   sha256 "2db22d4f84b2e2198a427fb967b993e4356db09c0a18cfb3aef1aa5a7eb77459"
@@ -8,7 +8,8 @@ class Wps2odt < Formula
   depends_on "libwps"
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
   end
