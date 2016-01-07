416c8a7a7a15562fe60720eb84db71eff7ffd7c7
Baptiste Fontaine <batifon@yahoo.fr>
2016-01-07 11:59:55 +0100

    htqc: import from homebrew/science
    The current URL gives a 404 and the latest release doesn’t build.
    
    Closes #83.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

---
+++ htqc.rb
@@ -0,0 +1,25 @@
+class Htqc < Formula
+  homepage "http://sourceforge.net/projects/htqc/"
+  # doi "10.1186/1471-2105-14-33"
+  # tag "bioinformatics"
+
+  url "https://downloads.sourceforge.net/project/htqc/htqc-0.90.2-Source.tar.gz"
+  sha1 "c4204ed1b85d78daa3c90968b99eea113308ed72"
+
+  depends_on "cmake" => :build
+  depends_on "boost"
+
+  fails_with :clang do
+    build 600
+    cause "error: call to constructor of 'htio::FastaIO' is ambiguous"
+  end
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/ht-stat", "--version"
+  end
+end
