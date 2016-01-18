5783deb085005a6c9519a3b56e68f615254f7bef
Baptiste Fontaine <batifon@yahoo.fr>
2016-01-18 16:17:36 +0100

    darwinbuild: import from head-only
    It doesn't build on >10.9; It’s unlikely upstream fixes it as the latest
    commit was almost two years ago and the latest homepage update was six
    years ago.
    
    Closes #89.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

---
+++ darwinbuild.rb
@@ -0,0 +1,29 @@
+class Darwinbuild < Formula
+  homepage "http://darwinbuild.macosforge.org/"
+  head "http://svn.macosforge.org/repository/darwinbuild/trunk/"
+
+  patch :DATA
+
+  depends_on :xcode => :build
+
+  def install
+    xcodebuild "-configuration", "Release", "install", "DSTROOT=/", "PREFIX=#{prefix}", "SYMROOT=build"
+  end
+end
+
+__END__
+diff --git a/common.mk b/common.mk
+index 424109b..56fad54 100644
+--- a/common.mk
++++ b/common.mk
+@@ -15,9 +15,9 @@ BINDIR=$(DESTDIR)$(PREFIX)/bin
+ DATDIR=$(DESTDIR)$(PREFIX)/share
+ INCDIR=$(DESTDIR)$(PREFIX)/include
+ INSTALL=install
+-INSTALL_EXE_FLAGS=-m 0755 -o root -g wheel
++INSTALL_EXE_FLAGS=-m 0755
+ INSTALL_DIR_FLAGS=$(INSTALL_EXE_FLAGS)
+-INSTALL_DOC_FLAGS=-m 0644 -o root -g wheel
++INSTALL_DOC_FLAGS=-m 0644
+
+ SED=/usr/bin/sed
