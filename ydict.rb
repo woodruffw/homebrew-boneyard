2a675523f2fb48e1d935efcb21006e8fe0a87bea
Baptiste Fontaine <batifon@yahoo.fr>
2016-02-10 14:26:09 +0100

    ydict: bottle unneeded

--- ydict.rb
+++ ydict.rb
@@ -4,6 +4,8 @@ class Ydict < Formula
   url "https://github.com/chenpc/ydict/archive/1.3.4.tar.gz"
   sha256 "f542e876d268e09d2a6883529a76937a1f38d0e594aa056b33ec4a1c79a1b8c2"
 
+  bottle :unneeded
+
   def install
     bin.install "ydict" => "ydict.py"
     (bin/"ydict").write <<-EOS.undent
