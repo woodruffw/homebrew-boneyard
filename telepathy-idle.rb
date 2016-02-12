c8709e423a526b65b39493fcb40631644d8a618d
Dominyk Tiller <dominyktiller@gmail.com>
2016-02-12 23:27:23 +0000

    telepathy-idle: migrate to boneyard

---
+++ telepathy-idle.rb
@@ -0,0 +1,20 @@
+class TelepathyIdle < Formula
+  desc "Telepathy IRC connection manager"
+  homepage "http://telepathy.freedesktop.org/wiki/Components/"
+  url "http://telepathy.freedesktop.org/releases/telepathy-idle/telepathy-idle-0.2.0.tar.gz"
+  sha256 "3013ad4b38d14ee630b8cc8ada5e95ccaa849b9a6fe15d2eaf6d0717d76f2fab"
+
+  depends_on "pkg-config" => :build
+  depends_on "telepathy-glib"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --disable-debug
+      --disable-dependency-tracking
+    ]
+
+    system "./configure", *args
+    system "make", "install"
+  end
+end
